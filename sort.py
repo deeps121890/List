db0a8feaa94dc50d3db7d1b4fe65894f5e2862e5
deeps121890 <deepikadeepu8818@gmail.com>
2024-10-03 02:09:49 -0700

    Update sort.py

--- sort.py
+++ sort.py
@@ -5,3 +5,16 @@ Sample Input:
 Sample Output:
 10 20 30 40 50 
 '''
+# Function to sort and print the list
+def sort_and_print_list():
+    # Input the list elements in a single line
+    elements = list(map(int, input().split()))  # Read the input and convert to a list of integers
+
+    # Sort the list
+    elements.sort()  # Sort the list in ascending order
+
+    # Print the sorted list
+    print(" ".join(map(str, elements)))  # Join the sorted list into a string for output
+
+# Call the function
+sort_and_print_list()
