c008b4b7a8aaabf1751571487ee988c9ce30954c
deeps121890 <deepikadeepu8818@gmail.com>
2024-10-03 02:08:15 -0700

    Update rev.py

--- rev.py
+++ rev.py
@@ -5,3 +5,13 @@ Sample Input:
 Sample Output:
 50 40 30 20 10 
 '''
+# Function to print the list in reverse order
+def print_reverse_list():
+    # Input the list elements in a single line
+    elements = list(map(int, input().split()))  # Read the input and convert to a list of integers
+
+    # Print the elements in reverse order
+    print(" ".join(map(str, elements[::-1])))  # Reverse the list and join for output
+
+# Call the function
+print_reverse_list()
